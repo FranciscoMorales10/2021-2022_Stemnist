8dc436078325814b96707515a802e358c9a8dace
GbStem16556 <90982562+GbStem16556@users.noreply.github.com>
2022-01-10 19:27:11 -0600

    Update Mort.java

--- Mort.java
+++ Mort.java
@@ -3,35 +3,43 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name = "Mort (Blocks to Java)", group = "")
-public class Mort extends LinearOpMode {
+@TeleOp(name = "Mort13 (Blocks to Java)")
+public class Mort13 extends LinearOpMode {
 
+  private Servo armServo;
   private DcMotor frontright;
   private DcMotor frontleft;
   private DcMotor backright;
   private DcMotor backleft;
+  private DcMotor armMotor;
+  private DcMotor carouselMotor;
 
   /**
    * This function is executed when this Op Mode is selected from the Driver Station.
    */
   @Override
   public void runOpMode() {
-    double targetPower2;
+    float targetPower2;
 
-    frontright = hardwareMap.dcMotor.get("front right");
-    frontleft = hardwareMap.dcMotor.get("front left");
-    backright = hardwareMap.dcMotor.get("back right");
-    backleft = hardwareMap.dcMotor.get("back left");
+    armServo = hardwareMap.get(Servo.class, "armServo");
+    frontright = hardwareMap.get(DcMotor.class, "front right");
+    frontleft = hardwareMap.get(DcMotor.class, "front left");
+    backright = hardwareMap.get(DcMotor.class, "back right");
+    backleft = hardwareMap.get(DcMotor.class, "back left");
+    armMotor = hardwareMap.get(DcMotor.class, "armMotor");
+    carouselMotor = hardwareMap.get(DcMotor.class, "carouselMotor");
 
     waitForStart();
+    armServo.setPosition(1);
     if (opModeIsActive()) {
       // Put run blocks here.
       while (opModeIsActive()) {
         // forward and backwards
-        targetPower2 = -gamepad1.left_stick_y;
-        frontright.setPower(targetPower2);
         targetPower2 = gamepad1.left_stick_y;
+        frontright.setPower(targetPower2);
+        targetPower2 = -gamepad1.left_stick_y;
         frontleft.setPower(targetPower2);
         targetPower2 = -gamepad1.left_stick_y;
         backright.setPower(targetPower2);
@@ -39,28 +47,37 @@ public class Mort extends LinearOpMode {
         backleft.setPower(targetPower2);
         telemetry.update();
         // left and right
-        targetPower2 = -gamepad1.left_stick_x;
+        targetPower2 = gamepad1.right_stick_x;
         frontright.setPower(targetPower2);
-        targetPower2 = -gamepad1.left_stick_x;
+        targetPower2 = gamepad1.right_stick_x;
         frontleft.setPower(targetPower2);
-        targetPower2 = -gamepad1.left_stick_x;
+        targetPower2 = gamepad1.right_stick_x;
         backright.setPower(targetPower2);
-        targetPower2 = -gamepad1.left_stick_x;
+        targetPower2 = gamepad1.right_stick_x;
         backleft.setPower(targetPower2);
         telemetry.update();
         // turning
-        targetPower2 = -gamepad1.right_stick_x;
+        targetPower2 = -gamepad1.left_stick_x;
         frontright.setPower(targetPower2);
-        targetPower2 = -gamepad1.right_stick_x;
+        targetPower2 = -gamepad1.left_stick_x;
         frontleft.setPower(targetPower2);
-        targetPower2 = gamepad1.right_stick_x;
+        targetPower2 = gamepad1.left_stick_x;
         backright.setPower(targetPower2);
-        targetPower2 = gamepad1.right_stick_x;
+        targetPower2 = gamepad1.left_stick_x;
         backleft.setPower(targetPower2);
-        // Autonomous
-        
-        
-        // Sensors
+        // Carousel Motor 
+        targetPower2 = gamepad2.right_stick_y;
+        carouselMotor.setPower(targetPower2);
+        // arm motor
+        targetPower2 = gamepad2.left_stick_y / 2;
+        armMotor.setPower(targetPower2);
+        // servor
+        if (gamepad2.a) {
+          armServo.setPosition(0);
+        } else {
+          armServo.setPosition(0.5);
+        }
+        // idk
       }
     }
   }
